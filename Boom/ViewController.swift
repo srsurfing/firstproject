b96c150974b9e152d5a1bcf2431a9774510b40f2
Gordon Ling <gordonling@Gordons-MacBook-Air.local>
2016-07-23 16:50:34 -0700

    added files for all of them

--- Boom/ViewController.swift
+++ Boom/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redBomb: UIImageView!
+    @IBOutlet weak var blueBomb: UIImageView!
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func blueButton(sender: AnyObject) {
+        
+        blueBomb.hidden = true
+        
+        
+    }
 
+    @IBAction func redButton(sender: AnyObject) {
+        
+        redBomb.hidden = true
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
